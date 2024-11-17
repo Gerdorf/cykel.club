fe64c8bd1ededcc6df5c1e8d70677af6443fed3d
Ludvig Gerdorf <gerdorfludvig@gmail.com>
2024-11-17 13:45:33 +0100

    Updated with favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
